1eba8d9425742e0337234cae3743444b1904b640
u-s <myagkov-a@yandex.ru>
2016-03-08 11:31:49 +0300

    fix daemon

--- main.cpp
+++ main.cpp
@@ -38,10 +38,10 @@ int main(int argc, char* argv[])
       }
     }
 
-   // if (daemon(0, 0) == -1) {
-   //   perror("daemon failed");
-   //   return -1;
-   // }
+    if (daemon(0, 0) == -1) {
+      perror("daemon failed");
+      return -1;
+    }
     
 
     thread_pool pool(boost::thread::hardware_concurrency());
